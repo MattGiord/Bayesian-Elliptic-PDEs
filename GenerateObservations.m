7289ba9c23c3794a99f42a2f9384d66f6c5e2cd3
Matteo Giordano <145664375+MattGiord@users.noreply.github.com>
2025-03-19 13:04:27 +0100

    Add files via upload

--- GenerateObservations.m
+++ GenerateObservations.m
@@ -1,22 +1,24 @@
-% Copyright (c) 2023 Matteo Giordano
+% Copyright (c) 2025 Matteo Giordano
 %
-% Codes accompanying the article "Bayesian nonparametric inference in 
-% PDE models: asymptotic theory and implementation" by Matteo Giordano
+% Codes accompanying the article "Bayesian nonparametric inference in elliptic 
+% PDEs: Convergence rates and implementation" by Matteo Giordano
 
 %%
-% Let O be the disk (in R^2) with radius r. Consider the 2D elliptic PDE in
+% Discrete noisy observations of elliptic PDE solution
+%
+% Let O be a smooth domain in R^2. Consider the 2D elliptic PDE in
 % divergence form with homogeneous Dirichlet boundary conditions:
 %
-%   div(f_0 grad u)=s, in O
+%   div(f_0 grad u)=s_0, in O
 %   u=0, on boundary of O
 %
-% where s:O -> R is a given C^\infty(O) source function and 
-% f_0:O -> [f_min,+infty), f_min>0, is the unknown (sufficiently smooth)
+% where s_0:O -> R is the (sufficiently smooth) source function and 
+% f_0:O -> [f_min,+infty), f_min>0, is the (sufficiently smooth)
 % diffusivity.
 %
 % There exists a unique classical solution G(f_0)=u_f0. We observe data
 %
-%   Y_i = u_f0(X_i) + sigma W_i,   i = 1,...,n
+%   Y_i = u_{f0,s_0}(X_i) + sigma W_i,   i = 1,...,n
 %
 % where X_i are uniform random locations in O, sigma>0 and W_i are i.i.d. 
 % N(0,1) random variables.
@@ -24,119 +26,199 @@
 % The following code generates n observations (Y_i,X_i), i=1,...,n
 
 %%
-% Specify true diffusivity f_0
+% Create domain and triangular mesh
 
 % Display more digits
 format long
 
-% Specify radius of disk
-r = 1/sqrt(pi);
+ax_h = 1; 
+    % length of horizontal semiaxis
+ax_v = .75; 
+    % length of vertical semiaxis
+rot = pi/6;
+    % angle of rotation
+t = linspace(0,2*pi,1000);
+pgon = polyshape({ax_h*cos(t)*cos(rot) - ax_v*sin(t)*sin(rot)},...
+    {ax_v*sin(t)*cos(rot) + ax_h*cos(t)*sin(rot)});
+vol = pi*ax_h*ax_v;
+
+% Create a triangulation representation of pgon
+tr = triangulation(pgon);
+
+% Create a PDE model
+model = createpde;
+
+% With the triangulation data as a mesh, use the geometryFromMesh function
+% to create a geometry
+tnodes = tr.Points';
+telements = tr.ConnectivityList';
+geometryFromMesh(model,tnodes,telements);
+%pdegplot(model)
+
+% Generate and plot triangular mesh
+generateMesh(model,'Hmax',.05);
+%figure()
+%axes('FontSize', 20, 'NextPlot','add')
+%pdemesh(model)
+%xticks([-1,-.5,0,.5,1])
+%yticks([-1,-.5,0,.5,1])
+%xlabel('x', 'FontSize', 20);
+%ylabel('y', 'FontSize', 20);
+mesh_nodes = model.Mesh.Nodes; 
+    % 2 x mesh_nodes_num matrix whose columns contain the (x,y) coordinates 
+    % of the nodes in the mesh
+mesh_nodes_num = size(mesh_nodes); 
+mesh_nodes_num=mesh_nodes_num(2); 
+    % number of nodes in the mesh
+mesh_elements = model.Mesh.Elements; 
+    % 6 x mesh_elements_num whose columns contain the 6 node indices 
+    % identifying each triangle. The first 3 elements of each column contain 
+    % the indices of the 3 vertices of the triangle 
+mesh_elements_num = size(mesh_elements); 
+mesh_elements_num = mesh_elements_num(2); 
+    % number of triangles in the mesh
+[~,mesh_elements_area] = area(model.Mesh); 
+
+% Compute barycenters of triangular mesh elements
+barycenters = zeros(2,mesh_elements_num);
+for i=1:mesh_elements_num
+    barycenters(:,i) = mean(mesh_nodes(:,mesh_elements(1:3,i)),2);
+end
+
+%%
+% Specify true diffusivity f_0
 
 % Specify f_0 as a function of (x,y)
 f_min = 1;
-f0 = @(x,y) f_min + exp(-(10*x-2.5).^2-(10*y-2.5).^2)...
-    + exp(-(10*x-2.5).^2-(10*y+2.5).^2)...
-    + exp(-(10*x+2.5).^2-(10*y+2.5).^2)...
-    +exp(-(10*x+2.5).^2-(10*y-2.5).^2);
+% Main experiment
+f0 = @(x,y) f_min + exp(-(5*x-1.75).^2-(5*y-1.75).^2)...
+    + exp(-(5*x-1.75).^2-(5*y+1.75).^2)...
+    + exp(-(5*x+1.75).^2-(5*y+1.75).^2)...
+    +exp(-(5*x+1.75).^2-(5*y-1.75).^2);
 
-% Plot f_0 (possibly as a surface)
-nRhos = linspace(0,r,500) ;
-nThetas = linspace(0,2*pi,500) ;
-[Rhos, Thetas] = meshgrid(nRhos,nThetas) ;
-xx = Rhos.*cos(Thetas); 
-yy = Rhos.*sin(Thetas);
-f0_surf = f_min + exp(-(10*xx-2.5).^2-(10*yy-2.5).^2)...
-    + exp(-(10*xx-2.5).^2-(10*yy+2.5).^2)...
-    + exp(-(10*xx+2.5).^2-(10*yy+2.5).^2)...
-    + exp(-(10*xx+2.5).^2-(10*yy-2.5).^2);
+% Further experiments
+%f0 = @(x,y) f_min + exp(-(4*x-1.75).^2-(4*y-1).^2)...
+%    +exp(-(4*x+1.75).^2-(4*y+1).^2);
+%f0 = @(x,y) f_min + exp(-(5*x-1.75).^2-(2.5*y-.25).^2)...
+%    +exp(-(5*x+1.75).^2-(5*y+1).^2);
+%f0 = @(x,y) f_min + .5*exp(-(5*x-1.75).^2-(5*y-1.75).^2)...
+%    -.5*exp(-(5*x+1.75).^2-(5*y+1.75).^2);
+f0_mesh = f0(mesh_nodes(1,:),mesh_nodes(2,:));
+f0_bary = f0(barycenters(1,:),barycenters(2,:));
+f0_norm=sqrt(sum(f0_bary.*mesh_elements_area));
+f0_fun=@(location,state) f0(location.x,location.y);
+    % specify diffusivity as a functions of (location,state) for elliptic PDE solver
+
+% Plot f0
 figure()
-surf(xx, yy, f0_surf)
-title('True diffusivity f_0','FontSize',20)
-%l = light('Position',[-50 -15 29])
-%set(gca,'CameraPosition',[208 -50 7687])
-xlabel('x','FontSize', 15)
-ylabel('y', 'FontSize', 15)
-lighting phong
-shading interp
-colorbar EastOutside
-colormap jet % change colormap. Some options are parula (default), jet, 
-% hot, cool (default for pdeplot), winter
-view(0,90) % to set view from above. Set view(45,45) for view from side
-colorbar
+axes('FontSize', 20, 'NextPlot','add')
+pdeplot(model,'XYData',f0_mesh,'ColorMap',jet)
+colorbar('Fontsize',20)
+title('True diffusivity f_0','FontSize',20);
+xticks([-1,-.5,0,.5,1])
+yticks([-1,-.5,0,.5,1])
+xlabel('x', 'FontSize', 20);
+ylabel('y', 'FontSize', 20);
+crameri vik
+
+% Specify the unknown source s_0 as a function of (x,y)
+s0 = @(x,y) exp(-(5*x-2.5).^2-(5*y).^2)+exp(-(7.5*x).^2-(2.5*y).^2)...
+    +exp(-(5*x+2.5).^2-(5*y).^2);
+s0_mesh=s0(mesh_nodes(1,:),mesh_nodes(2,:));
+s0_bary = s0(barycenters(1,:),barycenters(2,:));
+s0_fun=@(location,state) f0(location.x,location.y);
+    % specify diffusivity as a functions of (location,state) for elliptic PDE solver
+
+% Plot s0
+figure()
+axes('FontSize', 20, 'NextPlot','add')
+pdeplot(model,'XYData',s0_mesh,'ColorMap',jet)
+colorbar('Fontsize',20)
+title('True source s_0','FontSize',20);
+xticks([-1,-.5,0,.5,1])
+yticks([-1,-.5,0,.5,1])
+xlabel('x', 'FontSize', 20);
+ylabel('y', 'FontSize', 20);
+crameri vik
 
 %%
-% Elliptic PDE solution corresponding to true diffusivity f_0
-
-% Specify true diffusivity as a function of (location,state) 
-% to pass to elliptic PDE solver
-f0_fun=@(location,state) f0(location.x,location.y);
-
-% Create PDE model
-model = createpde();
-
-% Define circular domain O with centre (0,0) and radius r
-O = [1,0,0,r]';
-
-% Create geometry
-geom = decsg(O);
-geometryFromEdges(model,geom);
-
-% Plot geometry
-%figure()
-%pdegplot(model,'EdgeLabels','on')
-
-% Create mesh
-mesh = generateMesh(model,'Hmax',0.05); % reduce Hmax for finer mesh
-mesh=model.Mesh.Nodes;
-mesh_size=size(mesh);
-mesh_size=mesh_size(2);
-%figure()
-%pdeplot(model)
+% Elliptic PDE solution u_{f_0,s_0}
 
 % Specify zero Dirichlet boundary conditions on all edges
 applyBoundaryCondition(model,'dirichlet','Edge',1:model.Geometry.NumEdges,'u',0);
 
 % Specify the coefficients with c=f_0 and f=1 (the sintax for the source in MATLAB)
-specifyCoefficients(model,'m',0,'d',0,'c',f0_fun,'a',0,'f',1);
+specifyCoefficients(model,'m',0,'d',0,'c',f0_fun,'a',0,'f',s0_fun);
 
 % Solve PDE and plot solution
 results = solvepde(model);
 u0 = results.NodalSolution; 
 figure()
+axes('FontSize', 20, 'NextPlot','add')
 pdeplot(model,'XYData',u0)
-title('PDE solution G(f_0)\equiv u_{f_0}','FontSize', 20)
-xlabel('x','FontSize', 15)
-ylabel('y', 'FontSize', 15)
+title('PDE solution u_{f_0,s_0}','FontSize', 20)
+xlabel('x','FontSize', 20)
+ylabel('y', 'FontSize', 20)
+xticks([-1,-.5,0,.5,1])
+yticks([-1,-.5,0,.5,1])
+crameri batlowW
+
+% Compute norm of u_{f_0,s_0} for SNR
+u0_interp=scatteredInterpolant(mesh_nodes(1,:)',mesh_nodes(2,:)',u0);
+u0_bary=u0_interp(barycenters(1,:),barycenters(2,:));
+u0_norm = sqrt(sum((u0_bary).^2.*mesh_elements_area));
+%disp(['Norm of u0 = ', num2str(u0_norm)]);
 
 %%
 % Noisy observations of PDE solution and log-likelihood computation
 
+rng(1)
+
 % Sample design points and noise variales
-n=1000; % number of observations
-sigma=.001; % noise standard deviation
-rand_index=sort(randsample(mesh_size,n)); % random indices in the mesh
-rand_mesh=mesh(:,rand_index); % random sample of mesh points drawn 
-% uniformly at random
+n=1000; 
+    % number of observations
+sigma=.0025; 
+    % noise standard deviation
+disp(['Signal to noise ratio = ', num2str(u0_norm/sigma)])
+rand_index=sort(randsample(mesh_nodes_num,n));
+    % random indices in the mesh
+rand_mesh=mesh_nodes(:,rand_index); 
+    % random sample of mesh points drawn uniformly at random
+
 figure()
-scatter(rand_mesh(1,:), rand_mesh(2,:),'filled') % plot of sampled locations
-title('Random design points X_1,...,X_n','FontSize', 20)
-xlabel('x','FontSize', 15)
-ylabel('y', 'FontSize', 15)
+axes('FontSize', 20, 'NextPlot','add')
+scatter(rand_mesh(1,:),rand_mesh(2,:),'filled') 
+    % plot of sampled locations
+title('X_1,...,X_n','FontSize', 20)
+xticks([-1,-.5,0,.5,1])
+yticks([-1,-.5,0,.5,1])
+xlabel('x','FontSize', 20)
+ylabel('y', 'FontSize', 20)
 
 % Sample observations
-observations=u0(rand_index)+(mvnrnd(zeros(n,1),sigma^2*eye(n)))'; % add 
-% i.i.d N(0,sigma^2) noise to the observation
+observations=u0(rand_index)+(mvnrnd(zeros(n,1),sigma^2*eye(n)))'; 
+    % add i.i.d N(0,sigma^2) noise to the observation
 u0_corrupted=u0;
 u0_corrupted(rand_index)=observations;
 
 % Plot corrupted PDE solution
 figure()
+axes('FontSize', 20, 'NextPlot','add')
 pdeplot(model,'XYData',u0_corrupted)
-title('Observations Y_i=u_{f_0}(X_i)+\sigma W_i','FontSize', 20);
-xlabel('x','FontSize', 15)
-ylabel('y', 'FontSize', 15)
+title('Observations Y_i=u_{f_0,s_0}(X_i)+\sigma W_i','FontSize', 20);
+xlabel('x','FontSize', 20)
+ylabel('y', 'FontSize', 20)
+xticks([-1,-.5,0,.5,1])
+yticks([-1,-.5,0,.5,1])
+crameri batlowW
 
 % Compute log-likelihood of f_0
 loglik0=-sum((observations-u0(rand_index)).^2 )/(2*sigma^2);
 disp(['Log-likelihood of f_0 = ', num2str(loglik0)])
 
+% Compare likelihood with constant diffusivity for comparison
+specifyCoefficients(model,'m',0,'d',0,'c',1,'a',0,'f',s0_fun);
+results = solvepde(model);
+u_const_diff = results.NodalSolution;
+loglik_const_diff=-sum((observations-u_const_diff(rand_index)).^2 )/(2*sigma^2);
+disp(['Log-likelihood of constant f =', num2str(loglik_const_diff)])
--- pCNMatern.m
+++ pCNMatern.m
@@ -1,12 +1,12 @@
-% Copyright (c) 2023 Matteo Giordano
+% Copyright (c) 2025 Matteo Giordano
 %
-% Codes accompanying the article "Bayesian nonparametric inference in 
-% PDE models: asymptotic theory and implementation" by Matteo Giordano
+% Codes accompanying the article "Bayesian nonparametric inference in elliptic 
+% PDEs: Convergence rates and implementation" by Matteo Giordano
 
 %%
 % Bayesian nonparametric inference for the diffusivity f:O->[f_min,+infty) 
 % with Matérn process priors via the pCN algorithm
-
+%
 % Requires output of GenerateObservations.m (including f_0, observations 
 % and geometry), and the file K_mat.m (Matérn kernel)
 
@@ -14,29 +14,41 @@
 % Mesh for discretisation of parameter space via piecewise linear functions
 
 model_prior = createpde(); 
-geometryFromEdges(model_prior,geom);
-mesh_prior = generateMesh(model_prior,'Hmax',0.05);
-mesh_prior=model_prior.Mesh.Nodes;
-M=size(mesh_prior); 
-M=M(2); % discretised parameter space dimension
+geometryFromMesh(model_prior,tnodes,telements);
+generateMesh(model_prior,'Hmax',0.075);
+mesh_nodes_prior=model_prior.Mesh.Nodes;
+mesh_nodes_num_prior=size(mesh_nodes_prior); 
+mesh_nodes_num_prior=mesh_nodes_num_prior(2);
+    % discretised parameter space dimension
+mesh_elements_prior = model_prior.Mesh.Elements;
+mesh_elements_num_prior = size(mesh_elements_prior); 
+mesh_elements_num_prior = mesh_elements_num_prior(2); 
+[~,mesh_elements_area_prior] = area(model_prior.Mesh);
+
+% Compute barycenters of triangular mesh elements
+barycenters_prior = zeros(2,mesh_elements_num_prior);
+for i=1:mesh_elements_num_prior
+    barycenters_prior(:,i) = mean(mesh_nodes_prior(:,mesh_elements_prior(1:3,i)),2);
+end
+
+f0_mesh_prior = f0(mesh_nodes_prior(1,:),mesh_nodes_prior(2,:));
+f0_bary_prior = f0(barycenters_prior(1,:),barycenters_prior(2,:));
+F0_mesh_prior = log(f0_mesh_prior);
+F0_bary_prior=log(f0_bary_prior);
 
 %%
 % pCN initialisation
 
 % pCN initialisation at 0
-f_init=@(location,state) f_min+exp(0*location.x);
-F_init_num = 0*mesh_prior(1,:); % specify the initialisation point
-% as numeric vector
+f_init=@(location,state) exp(0*location.x);
+F_init_mesh_prior = 0*mesh_nodes_prior(1,:); 
 
 % pCN initialisation at f0
 %f_init=@(location,state) f0(location.x,location.y);
-%F_init_num = log(exp(-(10*mesh_prior(1,:)-2.5).^2-(10*mesh_prior(2,:)-2.5).^2) ...
-%    + exp(-(10*mesh_prior(1,:)-2.5).^2-(10*mesh_prior(2,:)+2.5).^2) ...
-%    + exp(-(10*mesh_prior(1,:)+2.5).^2-(10*mesh_prior(2,:)+2.5).^2) ...
-%    + exp(-(10*mesh_prior(1,:)+2.5).^2-(10*mesh_prior(2,:)-2.5).^2));
+%F_init_mesh_prior = F0_mesh_prior;
 
 % Solve elliptic PDE for initial pCN state
-specifyCoefficients(model,'m',0,'d',0,'c',f_init,'a',0,'f',1);
+specifyCoefficients(model,'m',0,'d',0,'c',f_init,'a',0,'f',s0_fun);
 results_current = solvepde(model);
 u_current = results_current.NodalSolution; 
 
@@ -49,94 +61,83 @@ disp(['Log-likelihood of f_0 = ', num2str(loglik0)])
 %%
 % Prior covariance matrix for prior draws
 
+M = mesh_nodes_num_prior;
 % Compute covariance matrix for Matérn process on mesh for discretisation
-prior_regularity = 10; l = .125; % hyper-parameters in Mtern kernel
+prior_regularity = 3; l = .25; % hyper-parameters in Mtern kernel
 Cov_matr = zeros(M,M);
 for i=1:M
     for j=i:M
-        Cov_matr(i,j) = K_mat(mesh_prior(:,i),mesh_prior(:,j),prior_regularity,l);
+        Cov_matr(i,j) = K_mat(mesh_nodes_prior(:,i),mesh_nodes_prior(:,j),prior_regularity,l);
         Cov_matr(j,i) = Cov_matr(i,j);
     end
 end
+scaling = n^(-2/(2*prior_regularity+2+4));
+    % n dependent scaling of covariance
+Cov_matr = scaling^2*Cov_matr;
 
-%%
+rng(1)
 % Sample and plot a prior draw
-
 prior_draw = mvnrnd(zeros(M,1),Cov_matr,1)';
+f_rand = exp(prior_draw);
 figure()
-tri = delaunay(mesh_prior(1,:),mesh_prior(2,:));
-subplot(1,2,1)
-trisurf(tri,mesh_prior(1,:),mesh_prior(2,:),prior_draw);
-title('F\sim \Pi(\cdot)','FontSize',20)
-xlabel('x','FontSize', 15)
-ylabel('y', 'FontSize', 15)
-%l = light('Position',[-50 -15 29])
-%set(gca,'CameraPosition',[208 -50 7687])
-lighting phong
-shading interp
-colorbar EastOutside
-colormap jet
-view(0,90)
-colorbar
-subplot(1,2,2)
-trisurf(tri,mesh_prior(1,:),mesh_prior(2,:),f_min+exp(prior_draw));
-title('f=f_{min}+exp(F)','FontSize',20)
-xlabel('x','FontSize', 15)
-ylabel('y', 'FontSize', 15)
-%l = light('Position',[-50 -15 29])
-%set(gca,'CameraPosition',[208 -50 7687])
-lighting phong
-shading interp
-colorbar EastOutside
-colormap jet
-view(0,90)
-colorbar
+axes('FontSize', 20, 'NextPlot','add')
+pdeplot(model_prior,'XYData',f_rand,'ColorMap',jet);
+title('f=e^F, F\sim\Pi','FontSize',20)
+colorbar('FontSize',20)
+xlabel('x','FontSize', 20)
+ylabel('y', 'FontSize', 20)
+xticks([-1,-.5,0,.5,1])
+yticks([-1,-.5,0,.5,1])
+crameri vik
 
 %%
 % pCN algorithm
 
 % Parameters for pCN algorithms
-delta=.00125; % smaller delta imply shorter moves for the pCN proposal
-MCMC_length=10000; % number of pCN draws
+delta=.00025; 
+    % smaller delta imply shorter moves for the pCN proposal
+MCMC_length=250; 
+    % number of pCN draws
 
 % Trackers
-accept_count = zeros(1,MCMC_length); % initialise vector % to keep track 
-% of acceptance steps
-alphas = ones(1,MCMC_length); % initialise vector to keep  track of 
-% acceptance probability
-unifs = rand(1,MCMC_length); % i.i.d. Un(0,1) random variables for 
-% Metropolis-Hastings updates
-F_MCMC = zeros(M,MCMC_length); % initialise marix to store the MCMC chain 
-% of functions
-F_MCMC(:,1)=F_init_num; % set pCN initialisation point
-loglik_MCMC = zeros(1,MCMC_length); % initialise vector to keep track of 
-% the loglikelihood of the pCN chain
-loglik_MCMC(1) = loglik_init; % set initial loglikelihood for 
-% initialisation point
+accept_count = zeros(1,MCMC_length); 
+    % initialise vector % to keep track of acceptance steps
+alphas = ones(1,MCMC_length); 
+    % initialise vector to keep  track of acceptance probability
+unifs = rand(1,MCMC_length); 
+    % i.i.d. Un(0,1) random variables for Metropolis-Hastings updates
+F_MCMC = zeros(M,MCMC_length); 
+    % initialise marix to store the MCMC chain of functions
+F_MCMC(:,1)=F_init_mesh_prior; 
+    % set pCN initialisation point
+loglik_MCMC = zeros(1,MCMC_length); 
+    % initialise vector to keep track of the loglikelihood of the pCN chain
+loglik_MCMC(1) = loglik_init; 
+    % set initial loglikelihood for initialisation point
 loglik_current = loglik_MCMC(1);
 F_current_num = F_MCMC(:,1);
 
 tic
 
 for MCMC_index=2:MCMC_length
-    disp(["MCMC step n." num2str(MCMC_index)])
+    fprintf('pCN step n. %d\n',MCMC_index);
     
     % Construct pCN proposal
     prior_draw = mvnrnd(zeros(M,1),Cov_matr,1)';
     F_proposal_num = sqrt(1-2*delta)*F_current_num + sqrt(2*delta)*prior_draw;
         
     % Define proposal diffusivity function to pass to PDE solver
-    F_proposal=@(location,state) griddata(mesh_prior(1,:),mesh_prior(2,:),F_proposal_num,location.x,location.y);
-    f_proposal=@(location,state) f_min+exp(F_proposal(location,state));
+    F_proposal=@(location,state) griddata(mesh_nodes_prior(1,:),mesh_nodes_prior(2,:),F_proposal_num,location.x,location.y);
+    f_proposal=@(location,state) exp(F_proposal(location,state));
 
     % Solve PDE with coefficient equal to the proposal
-    specifyCoefficients(model,'m',0,'d',0,'c',f_proposal,'a',0,'f',1);
+    specifyCoefficients(model,'m',0,'d',0,'c',f_proposal,'a',0,'f',s0_fun);
     results_proposal = solvepde(model);
     u_proposal = results_proposal.NodalSolution; 
 
     % Compute acceptance probability
     loglik_proposal = -sum( (observations-u_proposal(rand_index)).^2 )/(2*sigma^2);
-    alpha=exp(loglik_proposal-loglik_current)
+    alpha=exp(loglik_proposal-loglik_current);
     alphas(MCMC_index)=alpha;
 
     if unifs(MCMC_index)<alpha % if verified, accept proposal
@@ -150,9 +151,7 @@ for MCMC_index=2:MCMC_length
     loglik_MCMC(MCMC_index) = loglik_current;
 end
 
-toc % return run time. On MATLAB 2023a running on MacBook Pro M1 it takes 
-% ~2min for 1000 iterates, with the PDE solver using a mesh with 1981 nodes and 
-% a discretisation of the parameter space using a mesh of 1981 nodes
+toc
 
 %%
 % Acceptance and loglikelihood along the MCMC chain
@@ -161,88 +160,54 @@ n_accept_steps=accept_count(1,MCMC_length);
 accept_ratio=accept_count./(1:MCMC_length);
 
 figure()
-subplot(1,2,1)
+axes('FontSize', 20, 'NextPlot','add')
 plot(accept_ratio,'LineWidth',2)
 title('Acceptance ratio','FontSize',20)
-xlabel('MCMC step','FontSize', 15)
-subplot(1,2,2)
+xlabel('MCMC step','FontSize', 20)
+
+figure()
+axes('FontSize', 20, 'NextPlot','add')
 plot(loglik_MCMC,'LineWidth',2)
 %title('Loglikelihood along the MCMC chain','FontSize',20)
-xlabel('MCMC step','FontSize', 15)
+xlabel('MCMC step','FontSize', 20)
 yline(loglik0,'r','LineWidth',2)
-legend('Loglikelihood along MCMC chain','Loglikelihood of f_0','Fontsize',15)
+legend('Loglikelihood along MCMC chain','Loglikelihood of f_0','Fontsize',20)
 
 %%
 % MCMC average and estimation error
 
 % Compute MCMC average
-burnin=1000;
-F_mean_num = mean(F_MCMC(:,burnin+1:MCMC_length),2);
-f_mean_num=f_min+exp(F_mean_num);
-f0_mesh_prior = f_min + exp(-(10*mesh_prior(1,:)-2.5).^2-(10*mesh_prior(2,:)-2.5).^2) ...
-    + exp(-(10*mesh_prior(1,:)-2.5).^2-(10*mesh_prior(2,:)+2.5).^2)...
-    + exp(-(10*mesh_prior(1,:)+2.5).^2-(10*mesh_prior(2,:)+2.5).^2)...
-    + exp(-(10*mesh_prior(1,:)+2.5).^2-(10*mesh_prior(2,:)-2.5).^2);
+burnin=min(5000,MCMC_length/2);
+F_mean_mesh_prior = mean(F_MCMC(:,burnin+1:MCMC_length),2);
+f_mean_mesh_prior=exp(F_mean_mesh_prior);
 
-% Plot MCMC average and estimation erorr
-figure()
-ax1 = subplot(1,2,1);
-trisurf(tri,mesh_prior(1,:),mesh_prior(2,:),f0_mesh_prior) 
-shading interp
-colorbar EastOutside
-view(0,90) % to set view from above
-colorbar
-caxis([min(f0_mesh_prior), max(f0_mesh_prior)]);
-colormap(ax1,jet)
-title('True f_0','FontSize',20)
-ax2=subplot(1,2,2);
-trisurf(tri,mesh_prior(1,:),mesh_prior(2,:),f_mean_num)
-xlabel('x','FontSize', 15)
-ylabel('y', 'FontSize', 15)
-title('Posterior mean estimate (via pCN)','FontSize',20)
-shading interp
-colorbar EastOutside
-view(0,90)
-colorbar
-colormap(ax2,jet)
-caxis([min(f0_mesh_prior), max(f0_mesh_prior)]);
-xlabel('x','FontSize', 15)
-ylabel('y', 'FontSize', 15)
-
-figure()
-ax3 = subplot(1,1,1);
-trisurf(tri,mesh_prior(1,:),mesh_prior(2,:),f0_mesh_prior'-f_mean_num)
-xlabel('x','FontSize', 15)
-ylabel('y', 'FontSize', 15)
-title('Estimation Error','FontSize',20)
-%l = light('Position',[-50 -15 29]);
-%set(gca,'CameraPosition',[208 -50 7687]);
-%lighting phong
-shading interp
-colorbar EastOutside
-view(0,90) % to change view from above. Set view(0,90) for view from above
-colorbar
-colormap(ax3,hot)
-
-%%
 % Compute L^2-estimation error
+f_mean_bary_prior = griddata(mesh_nodes_prior(1,:),mesh_nodes_prior(2,:),f_mean_mesh_prior,barycenters_prior(1,:),barycenters_prior(2,:));
+estim_error = sqrt(sum((f0_bary_prior-f_mean_bary_prior).^2.*mesh_elements_area_prior));
+disp(['L^2 approximation estimation error = ', num2str(estim_error)])
+disp(['Relative error = ', num2str(estim_error/f0_norm)])
 
-mesh_prior_elements = model_prior.Mesh.Elements;
-mesh_prior_elements_num = size(mesh_prior_elements); 
-mesh_prior_elements_num = mesh_prior_elements_num(2); % number of triangular
-% mesh elements
-[~,elements_area] = area(model_prior.Mesh); % area of triangular mesh elements
+% Plot f_0 and posterior mean estimate
+figure()
+axes('FontSize', 20, 'NextPlot','add')
+pdeplot(model,'XYData',f0_mesh,'ColorMap',jet)
+clim([min(f0_mesh_prior),max(f0_mesh_prior)])
+colorbar('Fontsize',20)
+title('True conductivity f_0','FontSize',20);
+xticks([-1,-.5,0,.5,1])
+yticks([-1,-.5,0,.5,1])
+xlabel('x', 'FontSize', 20);
+ylabel('y', 'FontSize', 20);
+crameri vik
 
-% Compute barycenters of triangular mesh elements
-barycenters = zeros(2,mesh_prior_elements_num);
-for i=1:mesh_prior_elements_num
-    barycenters(:,i) = mean(mesh_prior(:,mesh_prior_elements(1:3,i)),2);
-end
-
-% Compute piecewise constant approximations of f_0 and posterior mean at
-% the triangle baricenters
-f0_interp = griddata(mesh_prior(1,:),mesh_prior(2,:),f0_mesh_prior,barycenters(1,:),barycenters(2,:));
-f_mean_interp = griddata(mesh_prior(1,:),mesh_prior(2,:),f_mean_num,barycenters(1,:),barycenters(2,:));
-
-% Approximate L^2 distance between f_0 and posterior mean
-estim_error = sqrt(sum((f0_interp-f_mean_interp).^2.*elements_area))
+figure()
+axes('FontSize', 20, 'NextPlot','add')
+pdeplot(model_prior,'XYData',f_mean_mesh_prior,'ColorMap',jet)
+clim([min(f0_mesh_prior),max(f0_mesh_prior)])
+colorbar('Fontsize',20)
+%title(['n = ',num2str(n),'; estimation error = ',num2str(estim_error)],'FontSize',20);
+xticks([-1,-.5,0,.5,1])
+yticks([-1,-.5,0,.5,1])
+xlabel('x', 'FontSize', 20);
+ylabel('y', 'FontSize', 20);
+crameri vik
